970bf9c182c162c4cbeeb2f13ca7f80f0aa292c9
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2018-08-11 03:40:28 +0700

    implement copy-to-clipboard

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,11 +14,15 @@
     </section>
     <div class="container is-fullhd">
       <section class="section">
-        <b-input v-model="keywords" size="is-large" placeholder="Search..." type="search" icon="magnify" />
+        <b-input v-model="keywords" size="is-large" placeholder="Search..." type="search" icon-pack="fas" icon="search" />
       </section>
       <section class="section">
         <div class="columns is-desktop is-multiline">
-          <div v-for="(emoji) in filteredEmojis" :key="emoji.key" class="column is-3 is-half-mobile float-left cursor-pointer">
+          <div
+            v-for="(emoji) in filteredEmojis"
+            :key="emoji.key"
+            class="column is-3 is-half-mobile float-left cursor-pointer hover:bg-grey-lightest"
+            @click="doCopy(`:${emoji.key}:`)">
             <div class="flex justify-center">
               <figure class="image is-64x64">
                 <img :src="emoji.image">
@@ -29,7 +33,7 @@
             </div>
           </div>
         </div>
-        <div v-if="filteredEmojis.length === 0" class="notification is-warning">
+        <div v-if="filteredEmojis.length === 0 && !isLoading" class="notification is-warning">
           <span v-if="keywords">No matching results</span>
           <span v-else>No data</span>
         </div>
@@ -46,13 +50,14 @@
       return {
         emojis: {},
         keywords: '',
-        url: 'https://api.github.com/emojis'
+        url: 'https://api.github.com/emojis',
+        isLoading: true
       }
     },
     computed: {
       filteredEmojis () {
         const keys = Object.keys(this.emojis)
-        return keys.filter(key => key.includes(this.keywords))
+        return keys.filter(key => key.includes(this.keywords.toLowerCase().trim()))
           .map(key => ({key, image: this.emojis[key]}))
       }
     },
@@ -60,7 +65,15 @@
       axios.get(this.url)
         .then(({ data }) => {
           this.emojis = data;
+          this.isLoading = false;
         })
+    },
+    methods: {
+      doCopy (text) {
+        this.$copyText(text).then(() => {
+          this.$toast.open('Copied!')
+        })
+      }
     }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Buefy from 'buefy'
+import VueClipboard from 'vue-clipboard2'
 
+Vue.use(VueClipboard)
 Vue.use(Buefy)
 
 Vue.config.productionTip = false
