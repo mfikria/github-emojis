244a0cee52b8debfd0cfb8e2ee304c72f991e075
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2018-08-11 01:54:40 +0700

    fix base url

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  baseUrl: '/github-emojis',
   outputDir: path.resolve(__dirname, "./docs"),
   chainWebpack: config => {
     config.resolve.alias
